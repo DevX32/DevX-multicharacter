e1205e7cfa1eac238439717d1cc4bac5d7dfa71f
DevX32 <60389339+DevX32@users.noreply.github.com>
2022-08-26 17:56:25 +0545

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -128,7 +128,7 @@ RegisterNUICallback('cDataPed', function(nData, cb)
                     PlaceObjectOnGroundProperly(charPed)
                     SetBlockingOfNonTemporaryEvents(charPed, true)
                     exports['fivem-appearance']:setPedAppearance(charPed, skinData)
-                RequestAnimDict("timetable@reunited@ig_10")
+                    RequestAnimDict("timetable@reunited@ig_10")
                         while not HasAnimDictLoaded("timetable@reunited@ig_10") do
                             Wait(1)
                         end		
