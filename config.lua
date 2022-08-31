b6f5a621a759f89b484069a7cf6428140be8bd67
DevX32 <60389339+DevX32@users.noreply.github.com>
2022-08-31 20:03:18 +0545

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,26 +1,9 @@
 Config = {}
 Config.StartingApartment = true -- Enable/disable starting apartments (make sure to set default spawn coords)
---Config.Interior = vector3(-787.65, 333.63, 217.03) -- Interior to load where characters are previewed
---Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
---Config.PedCoords = vector4(-784.4, 339.66, 216.84, 179.12) -- Create preview ped at these coordinates
---Config.HiddenCoords = vector4(-781.27, 315.68, 217.64, 85.22) -- Hides your actual ped while you are in selection
---Config.CamCoords = vector4(-788.66, 337.91, 217.02, 269.78) -- Camera coordinates for character preview screen --
---------------DevX32-----------------
---interior second
---Config.Interior = vector3(-755.32, 318.55, 221.88) -- Interior to load where characters are previewed
---Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
---Config.PedCoords = vector4(-757.82, 317.81, 221.88, 261.87) -- Create preview ped at these coordinates
---Config.HiddenCoords = vector4(-775.06, 320.47, 222.59, 83.87) -- Hides your actual ped while you are in selection
---Config.CamCoords = vector4(-756.44, 323.11, 222.39, 189.27) -- Camera coordinates for character preview screen
---------------DevX32----------------
---interior third
---Config.Interior = vector3(-784.9, 339.39, 187.11) -- Interior to load where characters are previewed
---Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
---Config.PedCoords = vector4(-784.24, 339.66, 187.11, 177.87) -- Create preview ped at these coordinates
---Config.HiddenCoords = vector4(-783.31, 319.82, 187.89, 301.25) -- Hides your actual ped while you are in selection
---Config.CamCoords = vector4(-788.61, 338.11, 187.36, 270.35) -- Camera coordinates for character preview screen
---------------DevX32----------------
---interior fourth
+Config.CorePrefix = 'QBCore'
+Config.CoreFolderName = 'qb-core' -------------folder name of core
+Config.oxmysql = 'new' -------- oxmysql new or old 
+-------------DevX32----------------
 Config.Interior = vector3(-781.52, 336.98, 211.23) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
 Config.PedCoords = vector4(-783.5, 338.63, 211.2, 268.21) -- Create preview ped at these coordinates
