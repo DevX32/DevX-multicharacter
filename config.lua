0a31b07ced7e9c5dd24601b88b3bca5389c198b3
DevX32 <60389339+DevX32@users.noreply.github.com>
2022-08-31 18:10:44 +0545

    Update config.lua

--- config.lua
+++ config.lua
@@ -27,3 +27,12 @@ Config.PedCoords = vector4(-783.5, 338.63, 211.2, 268.21) -- Create preview ped 
 Config.HiddenCoords = vector4(-764.91, 332.15, 211.4, 307.68) -- Hides your actual ped while you are in selection
 Config.CamCoords = vector4(-781.43, 334.48, 211.32, 1.03) -- Camera coordinates for character preview screen
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
+--configs--
+Config.ClothingEvent = 'qb-clothing:client:loadPlayerClothing'
+Config.UseFivemAppearance = true 
+
+Config.FivemAppaeranceversion = 'il' -------- if u are using AJ one do aj or if u are using illenium do il
+
+Config.FivemAppearanceName = 'fivem-appearance'
+
+Config.CreateCharcterFirstEvent = 'qb-clothes:client:CreateFirstCharacter
