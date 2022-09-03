fae4e8340ebf784a90bcd45177371a91a2892cb0
DevX32 <60389339+DevX32@users.noreply.github.com>
2022-09-03 12:07:12 +0545

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 Config = {}
 Config.StartingApartment = true -- Enable/disable starting apartments (make sure to set default spawn coords)
-Config.CorePrefix = 'QBCore'
-Config.CoreFolderName = 'qb-core' -------------folder name of core
-Config.oxmysql = 'new' -------- oxmysql new or old 
+--Config.CorePrefix = 'QBCore'
+--Config.CoreFolderName = 'qb-core' -------------folder name of core
+--Config.oxmysql = 'new' -------- oxmysql new or old 
 -------------DevX32----------------
 Config.Interior = vector3(-781.52, 336.98, 211.23) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
@@ -11,12 +11,12 @@ Config.HiddenCoords = vector4(-764.91, 332.15, 211.4, 307.68) -- Hides your actu
 Config.CamCoords = vector4(-781.43, 334.48, 211.32, 1.03) -- Camera coordinates for character preview screen
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
 --configs--
-Config.ClothingEvent = 'qb-clothing:client:loadPlayerClothing'
+--Config.ClothingEvent = 'qb-clothing:client:loadPlayerClothing'
 
-Config.UseFivemAppearance = false
+--Config.UseFivemAppearance = false
 
-Config.FivemAppearanceversion = 'il' -------- if u are using AJ one do aj or if u are using illenium do il
+--Config.FivemAppearanceversion = 'il' -------- if u are using AJ one do aj or if u are using illenium do il
 
-Config.FivemAppearanceName = 'fivem-appearance'
+--Config.FivemAppearanceName = 'fivem-appearance'
 
-Config.CreateCharcterFirstEvent = 'qb-clothes:client:CreateFirstCharacter
+--Config.CreateCharcterFirstEvent = 'qb-clothes:client:CreateFirstCharacter
