34e96c18c5f87772a6cd9ffc9ce1b80323b9b6f8
DevX32 <60389339+DevX32@users.noreply.github.com>
2022-09-04 18:58:13 +0545

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,5 @@
 Config = {}
 Config.StartingApartment = true -- Enable/disable starting apartments (make sure to set default spawn coords)
---Config.CorePrefix = 'QBCore'
---Config.CoreFolderName = 'qb-core' -------------folder name of core
---Config.oxmysql = 'new' -------- oxmysql new or old 
 -------------DevX32----------------
 Config.Interior = vector3(-781.52, 336.98, 211.23) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
@@ -10,13 +7,7 @@ Config.PedCoords = vector4(-783.5, 338.63, 211.2, 268.21) -- Create preview ped 
 Config.HiddenCoords = vector4(-764.91, 332.15, 211.4, 307.68) -- Hides your actual ped while you are in selection
 Config.CamCoords = vector4(-781.43, 334.48, 211.32, 1.03) -- Camera coordinates for character preview screen
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
---configs--
---Config.ClothingEvent = 'qb-clothing:client:loadPlayerClothing'
 
---Config.UseFivemAppearance = false
 
---Config.FivemAppearanceversion = 'il' -------- if u are using AJ one do aj or if u are using illenium do il
 
---Config.FivemAppearanceName = 'fivem-appearance'
 
---Config.CreateCharcterFirstEvent = 'qb-clothes:client:CreateFirstCharacter
