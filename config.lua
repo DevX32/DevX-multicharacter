a9e67119f49d18107d51d90e1b9543a4f86e36c8
DevX32 <60389339+DevX32@users.noreply.github.com>
2022-09-08 19:52:50 +0545

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,8 +6,6 @@ Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords
 Config.PedCoords = vector4(-783.5, 338.63, 211.2, 268.21) -- Create preview ped at these coordinates
 Config.HiddenCoords = vector4(-764.91, 332.15, 211.4, 307.68) -- Hides your actual ped while you are in selection
 Config.CamCoords = vector4(-781.43, 334.48, 211.32, 1.03) -- Camera coordinates for character preview screen
-Config.EnableDeleteButton = true -- Define if the player can delete the character or not
-
 
 
 
