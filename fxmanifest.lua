b6ae1cfa9abc373514581940c0f8fdcb754a2fea
DevX32 <60389339+DevX32@users.noreply.github.com>
2022-09-09 05:41:49 +0545

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,13 +3,11 @@ game 'gta5'
 
 description 'DevX-Multicharacter'
 
-shared_scripts 'config.lua'
-client_script {
-            'client/main.lua',
-}
+shared_script 'config.lua'
+client_script 'client/main.lua'
 server_scripts  {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua',
+    'server/main.lua'
 }
 
 ui_page 'html/index.html'
