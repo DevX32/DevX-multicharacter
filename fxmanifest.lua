4993d110dc1c30f6f00d8cc38f222bb36a896dee
DevX32 <60389339+DevX32@users.noreply.github.com>
2023-07-11 08:00:15 +0545

    update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,30 @@
 fx_version 'cerulean'
 game 'gta5'
-lua54 'yes'
-
+author 'DevX32'
 description 'DevX-Multicharacter'
-shared_script 'config.lua'
-client_script 'client/main.lua'
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared.lua'
+}
+client_script {
+    'client/cl_multi.lua'
+}
 server_scripts  {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+    '@qb-apartments/config.lua',
+    'server/sv_multi.lua'
 }
 
-ui_page 'html/index.html'
+ui_page 'web/ui.html'
 
 files {
-    'html/index.html',
-    'html/style.css',
-    'html/reset.css',
-    'html/profanity.js',
-    'html/script.js'
+    'web/ui.html',
+    'web/ui.css',
+    'web/reset.css',
+    'web/prompts.js',
+    'web/app.js'
 }
+dependencies {
+    'ox_lib'
+}
+lua54 'yes'
